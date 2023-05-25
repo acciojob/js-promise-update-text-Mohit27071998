6639cbe74ced28be0f69c89a9db580140447780d
Mohit Kamdi <130160983+Mohit27071998@users.noreply.github.com>
2023-05-25 15:49:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let prom  = new Promise((resolve,reject) =>{
+    console.log("Hello, world!")
+	
+},1000)
+
+let div = document.getElementById("output");
+div.innerText = prom;
